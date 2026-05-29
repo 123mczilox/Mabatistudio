2aecea754787e9bbac0eeb3c9c79cdef2044b814
Zidane Mczilox <wanjohizidane14@gmail.com>
2026-05-29 13:31:36 +0300

    fix05

--- core/supa_storage.py
+++ core/supa_storage.py
@@ -4,7 +4,6 @@ import os
 
 SUPABASE_URL = "https://juhkarrjmlwjwjtbysnn.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1aGthcnJqbWx3andqdGJ5c25uIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTk3MDU5OCwiZXhwIjoyMDk1NTQ2NTk4fQ.xpcHuc1Qojq308mOaiGEj6nM8CwviTvN2OMVO5hYAzM"
-
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
